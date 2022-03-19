cc3db510fed49c1d7e2baff332c2ac040160b3b3
Dauntae Stewart <dauntae9@gmail.com>
2022-03-18 23:03:22 -0500

    commit tree d5e7ccf6d29ffd2a7d77df3ad71cdc941780b0d6

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b) { 
+    return a + b;
+} add();
+    
+function subtract(c, d) {
+    return (c - d);
+} subtract();
+
+function multiply(e, f) {
+    return (e * f);
+} multiply();
+
+function divide(g, h) {
+    return (g / h);
+} divide();
+
+function increment(n){
+    return n + 1;
+}
+
+function decrement(n){
+    return n - 1;
+}
+
+parseInt("2", 10);
+parseInt("2.2222", 10);
+
+function makeInt(n, base){
+    return parseInt (n, 10);
+}
+
+function preserveDecimal(a){
+    return parseFloat(a);
+}
